d4f447e655df957d7c851db5a99796cd1f63024b
Artem Pasyugin <APasyugin@alfabank.ru>
2020-11-01 21:45:07 +0300

    Add menu list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,114 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function App() {
+  return (
+    <Router>
+    <div class='row'>
+      <div className="Menu-list">
+        <h2>Artem Pasyugin notes</h2>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About me</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <h2>Tag list</h2>
+        <ul>
+          <li>
+            <Link to="/android">Android</Link>
+          </li>
+          <li>
+            <Link to="/ansible">Ansible</Link>
+          </li>
+          <li>
+            <Link to="/concurrency">Cuncurrency</Link>
+          </li>
+          <li>
+            <Link to="/devops">Devops</Link>
+          </li>
+          <li>
+            <Link to="/docker">Docker</Link>
+          </li>
+          <li>
+            <Link to="/java">Java</Link>
+          </li>
+          <li>
+            <Link to="/linux">Linux</Link>
+          </li>
+          <li>
+            <Link to="/project-management">Project management</Link>
+          </li>
+        </ul>
+      </div>
+      <div class='column'>
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+      <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+        <Route path="/about">
+            <Page text="About"/>
+        </Route>
+        <Route path="/android">
+            <Page text="Android"/>
+        </Route>
+        <Route path="/ansible">
+            <Page text="Ansible"/>
+        </Route>
+        <Route path="/concurrency">
+            <Page text="Concurrency"/>
+        </Route>
+        <Route path="/devops">
+            <Page text="Devops"/>
+        </Route>
+        <Route path="/docker">
+            <Page text="Docker"/>
+        </Route>
+        <Route path="/java">
+            <Page text="Java"/>
+        </Route>
+        <Route path="/linux">
+            <Page text="Linux"/>
+        </Route>
+        <Route path="/project-management">
+            <Page text="Project management"/>
+        </Route>
+        </Switch>
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,4 +121,10 @@ function App() {
   );
 }
 
-export default App;
+function Page(props) {
+  return (
+    <div className="page App-header">
+      <h2>{props.text}</h2>
+    </div>
+  );
+}
